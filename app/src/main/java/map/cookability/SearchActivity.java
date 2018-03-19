6f98563df349e946760697466a8c2c76e459e394
VigneshwaranVenugopal <theonlyvignesh@gmail.com>
2018-03-19 02:24:12 -0700

    Working search

--- app/src/main/java/map/cookability/SearchActivity.java
+++ app/src/main/java/map/cookability/SearchActivity.java
@@ -1,26 +1,39 @@
 package map.cookability;
 
 import android.os.Bundle;
-
-
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class SearchActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     private EditText mSearchField;
     private ImageButton mSearchBtn;
@@ -38,22 +51,28 @@ public class SearchActivity extends AppCompatActivity {
         mSearchField = (EditText)findViewById(R.id.search_field);
         mSearchBtn = (ImageButton)findViewById(R.id.search_button);
 
-        mResultList = (RecyclerView)findViewById(R.id.card_recycler_view);
-        mResultList.setHasFixedSize(true);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        mResultList.setLayoutManager(layoutManager);
-
+//        mResultList = (RecyclerView)findViewById(R.id.card_recycler_view);
+//        mResultList.setHasFixedSize(true);
+//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+//        mResultList.setLayoutManager(layoutManager);
+//
         mSearchBtn.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view) {
-                firebaseRecipeSearch();
+                String ed_text = mSearchField.getText().toString().trim();
+                if(ed_text.isEmpty() || ed_text.length() == 0 || ed_text.equals("") || ed_text == null)
+                    mSearchField.setText("Enter String");
+                    else
+                getData();
+                InputMethodManager mgr = (InputMethodManager) getSystemService(getBaseContext().INPUT_METHOD_SERVICE);
+                mgr.hideSoftInputFromWindow(mSearchField.getWindowToken(), 0);
             }
         });
 
     }
 
-    private void firebaseRecipeSearch() {
+    /*private void firebaseRecipeSearch() {
         FirebaseRecyclerAdapter<Recipe, RecipeViewHolder> firebaseRecyclerAdapter
                 = new FirebaseRecyclerAdapter<Recipe, RecipeViewHolder>(
                         Recipe.class,
@@ -72,7 +91,7 @@ public class SearchActivity extends AppCompatActivity {
         };
 
         mResultList.setAdapter(firebaseRecyclerAdapter);
-    }
+    }*/
 
 
     // View Holder class
@@ -95,4 +114,49 @@ public class SearchActivity extends AppCompatActivity {
             Glide.with(getApplicationContext()).load(dishImg).into(dish_image);
         }
     }
+
+
+    public final void getData(){
+
+        db.collection("recipes")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            ArrayList<Recipe> eventList = new ArrayList<>();
+                            final ListView listView = (ListView) findViewById(R.id.lisr);
+                            for (DocumentSnapshot document : task.getResult()) {
+                                Recipe current = new Recipe(document.getId(),document.getData());
+                                String e = current.title;
+                                Log.d("Result", e + " => " + document.getData());
+                                if(current.title.contains(mSearchField.getText()))
+                                eventList.add(current);
+                            }
+                            RecipeAdapter adapter = new RecipeAdapter(getBaseContext(), eventList);
+                            listView.setAdapter(adapter);
+                        } else {
+                            Log.d("ERROR TAG", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+    }
+
+        List<Query> createSimpleQueries() {
+            List<Query> querys = new ArrayList<>();
+            CollectionReference recipes = db.collection("recipes");
+
+            // [START fs_simple_queries]
+            Query countryQuery = recipes.whereEqualTo("name", "noodles");
+            Query populationQuery = recipes.whereLessThan("population", 1000000L);
+            Query cityQuery = recipes.whereGreaterThanOrEqualTo("name", "San Francisco");
+            // [END fs_simple_queries]
+
+            querys.add(countryQuery);
+            querys.add(populationQuery);
+            querys.add(cityQuery);
+            return querys;
+        }
+
 }
