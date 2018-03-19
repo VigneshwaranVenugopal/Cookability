b469b7ab7adaf991427e8e50b90560cd043c853a
buzhidaoya <xjiang1@scu.edu>
2018-03-18 23:34:30 -0700

    deleted random code

--- app/src/main/java/map/cookability/SearchActivity.java
+++ app/src/main/java/map/cookability/SearchActivity.java
@@ -2,15 +2,11 @@ package map.cookability;
 
 import android.os.Bundle;
 
-import android.support.v4.view.MenuItemCompat;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SearchView;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -79,70 +75,6 @@ public class SearchActivity extends AppCompatActivity {
         mResultList.setAdapter(firebaseRecyclerAdapter);
     }
 
-//    private void initViews(){
-//        mRecyclerView = (RecyclerView)findViewById(R.id.card_recycler_view);
-//        mRecyclerView.setHasFixedSize(true);
-//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-//        mRecyclerView.setLayoutManager(layoutManager);
-//    }
-//    private void loadJSON(){
-//        Retrofit retrofit = new Retrofit.Builder()
-//                .baseUrl(BASE_URL)
-//                .addConverterFactory(GsonConverterFactory.create())
-//                .build();
-//        RequestInterface request = retrofit.create(RequestInterface.class);
-//        Call<JSONResponse> call = request.getJSON();
-//        call.enqueue(new Callback<JSONResponse>() {
-//            @Override
-//            public void onResponse(Call<JSONResponse> call, Response<JSONResponse> response) {
-//
-//                JSONResponse jsonResponse = response.body();
-//                mArrayList = new ArrayList<>(Arrays.asList(jsonResponse.getAndroid()));
-//                mAdapter = new DataAdapter(mArrayList);
-//                mRecyclerView.setAdapter(mAdapter);
-//            }
-//
-//            @Override
-//            public void onFailure(Call<JSONResponse> call, Throwable t) {
-//                Log.d("Error",t.getMessage());
-//            }
-//        });
-//    }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//
-//        getMenuInflater().inflate(R.menu.searchbar, menu);
-//
-//        MenuItem search = menu.findItem(R.id.action_search);
-//        SearchView searchView = (SearchView) MenuItemCompat.getActionView(search);
-//        search(searchView);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//
-//        return super.onOptionsItemSelected(item);
-//    }
-//
-//    private void search(SearchView searchView) {
-//
-//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-//            @Override
-//            public boolean onQueryTextSubmit(String query) {
-//
-//                return false;
-//            }
-//
-//            @Override
-//            public boolean onQueryTextChange(String newText) {
-//
-//                if (mAdapter != null) mAdapter.getFilter().filter(newText);
-//                return true;
-//            }
-//        });
-//    }
 
     // View Holder class
     class RecipeViewHolder extends RecyclerView.ViewHolder{
