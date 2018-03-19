22918d94de2a46a1e007f04cd07608988758c7d2
buzhidaoya <xjiang1@scu.edu>
2018-03-18 23:31:37 -0700

    searchActivity

--- app/src/main/java/map/cookability/Recipe.java
+++ app/src/main/java/map/cookability/Recipe.java
@@ -13,6 +13,9 @@ public class Recipe {
     public String nonveg;
     public String imageUrl;
     public String category;
+
+    public Recipe(){}
+
     Recipe(String name, Map<String,Object> data){
         documentName = name;
         title = (String) data.get("name");
@@ -22,4 +25,101 @@ public class Recipe {
         nonveg = (String) data.get("nonveg");
         category = (String) data.get("category");
     }
+
+    public String getDocumentName() {
+        return documentName;
+    }
+
+    public void setDocumentName(String documentName) {
+        this.documentName = documentName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSteps() {
+        return steps;
+    }
+
+    public void setSteps(String steps) {
+        this.steps = steps;
+    }
+
+    public String getIngridients() {
+        return ingridients;
+    }
+
+    public void setIngridients(String ingridients) {
+        this.ingridients = ingridients;
+    }
+
+    public String getNonveg() {
+        return nonveg;
+    }
+
+    public void setNonveg(String nonveg) {
+        this.nonveg = nonveg;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+//    private String dish, image, name, type;
+//
+//    public Recipe(String dish, String image, String name, String type) {
+//        this.dish = dish;
+//        this.image = image;
+//        this.name = name;
+//        this.type = type;
+//    }
+//
+//    public String getDish() {
+//        return dish;
+//    }
+//
+//    public void setDish(String dish) {
+//        this.dish = dish;
+//    }
+//
+//    public String getImage() {
+//        return image;
+//    }
+//
+//    public void setImage(String image) {
+//        this.image = image;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getType() {
+//        return type;
+//    }
+//
+//    public void setType(String type) {
+//        this.type = type;
+//    }
 }
--- app/src/main/java/map/cookability/SearchActivity.java
+++ app/src/main/java/map/cookability/SearchActivity.java
@@ -11,17 +11,18 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -29,79 +30,138 @@ public class SearchActivity extends AppCompatActivity {
     private ImageButton mSearchBtn;
     private RecyclerView mResultList;
 
+    private DatabaseReference mRecipeDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        mRecipeDatabase = FirebaseDatabase.getInstance().getReference("Users");
+
         mSearchField = (EditText)findViewById(R.id.search_field);
         mSearchBtn = (ImageButton)findViewById(R.id.search_button);
 
         mResultList = (RecyclerView)findViewById(R.id.card_recycler_view);
-    }
-
-    private void initViews(){
-       // mRecyclerView = (RecyclerView)findViewById(R.id.card_recycler_view);
-        mRecyclerView.setHasFixedSize(true);
+        mResultList.setHasFixedSize(true);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(layoutManager);
-    }
-    private void loadJSON(){
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        RequestInterface request = retrofit.create(RequestInterface.class);
-        Call<JSONResponse> call = request.getJSON();
-        call.enqueue(new Callback<JSONResponse>() {
-            @Override
-            public void onResponse(Call<JSONResponse> call, Response<JSONResponse> response) {
+        mResultList.setLayoutManager(layoutManager);
 
-                JSONResponse jsonResponse = response.body();
-                mArrayList = new ArrayList<>(Arrays.asList(jsonResponse.getAndroid()));
-                mAdapter = new DataAdapter(mArrayList);
-                mRecyclerView.setAdapter(mAdapter);
-            }
+        mSearchBtn.setOnClickListener(new View.OnClickListener(){
 
             @Override
-            public void onFailure(Call<JSONResponse> call, Throwable t) {
-                Log.d("Error",t.getMessage());
+            public void onClick(View view) {
+                firebaseRecipeSearch();
             }
         });
+
+        //initViews();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    private void firebaseRecipeSearch() {
+        FirebaseRecyclerAdapter<Recipe, RecipeViewHolder> firebaseRecyclerAdapter
+                = new FirebaseRecyclerAdapter<Recipe, RecipeViewHolder>(
+                        Recipe.class,
+                        R.layout.content_search,
+                        RecipeViewHolder.class,
+                        mRecipeDatabase
+        ){
 
-        getMenuInflater().inflate(R.menu.searchbar, menu);
-
-        MenuItem search = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView) MenuItemCompat.getActionView(search);
-        search(searchView);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void search(SearchView searchView) {
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-
-                return false;
-            }
 
             @Override
-            public boolean onQueryTextChange(String newText) {
+            protected void populateViewHolder(RecipeViewHolder viewHolder, Recipe model, int position) {
 
-                if (mAdapter != null) mAdapter.getFilter().filter(newText);
-                return true;
+                viewHolder.setDetails(model.getTitle(), model.getCategory(), model.getImageUrl());
+                //viewHolder.setDetails(model.getName(), model.getType(), model.getImage());
             }
-        });
+        };
+
+        mResultList.setAdapter(firebaseRecyclerAdapter);
+    }
+
+//    private void initViews(){
+//        mRecyclerView = (RecyclerView)findViewById(R.id.card_recycler_view);
+//        mRecyclerView.setHasFixedSize(true);
+//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+//        mRecyclerView.setLayoutManager(layoutManager);
+//    }
+//    private void loadJSON(){
+//        Retrofit retrofit = new Retrofit.Builder()
+//                .baseUrl(BASE_URL)
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .build();
+//        RequestInterface request = retrofit.create(RequestInterface.class);
+//        Call<JSONResponse> call = request.getJSON();
+//        call.enqueue(new Callback<JSONResponse>() {
+//            @Override
+//            public void onResponse(Call<JSONResponse> call, Response<JSONResponse> response) {
+//
+//                JSONResponse jsonResponse = response.body();
+//                mArrayList = new ArrayList<>(Arrays.asList(jsonResponse.getAndroid()));
+//                mAdapter = new DataAdapter(mArrayList);
+//                mRecyclerView.setAdapter(mAdapter);
+//            }
+//
+//            @Override
+//            public void onFailure(Call<JSONResponse> call, Throwable t) {
+//                Log.d("Error",t.getMessage());
+//            }
+//        });
+//    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//
+//        getMenuInflater().inflate(R.menu.searchbar, menu);
+//
+//        MenuItem search = menu.findItem(R.id.action_search);
+//        SearchView searchView = (SearchView) MenuItemCompat.getActionView(search);
+//        search(searchView);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    private void search(SearchView searchView) {
+//
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//
+//                if (mAdapter != null) mAdapter.getFilter().filter(newText);
+//                return true;
+//            }
+//        });
+//    }
+
+    // View Holder class
+    class RecipeViewHolder extends RecyclerView.ViewHolder{
+
+        View mView;
+
+        public RecipeViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setDetails(String dishName, String dishCate, String dishImg){
+            TextView dish_name = (TextView)mView.findViewById(R.id.dish_name_search);
+            TextView dish_category = (TextView)mView.findViewById(R.id.category_search);
+            ImageView dish_image = (ImageView)mView.findViewById(R.id.dish_image_search);
+
+            dish_name.setText(dishName);
+            dish_category.setText(dishCate);
+            Glide.with(getApplicationContext()).load(dishImg).into(dish_image);
+        }
     }
 }
